7f5569e64a9f67f1cdea645b94aeb9faeb59947a
Deepak Jakhar <jakhar.deepak3@gmail.com>
2020-05-20 19:00:42 +0530

    Adding Additional STUN Server

--- public/js/video.js
+++ public/js/video.js
@@ -12,7 +12,7 @@
   /** @type {RTCConfiguration} */
   const config = {
     'iceServers': [{
-      'urls': ['stun:stun.l.google.com:19302']
+      'urls': ['stun:stun.l.google.com:19302', 'stun:stun01.sipphone.com']
     }]
   };
 
